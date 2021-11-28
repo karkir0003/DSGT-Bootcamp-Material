dea3123b9d2dbff271d53ad05a150571bf1ca8c1
karkir0003 <54720987+karkir0003@users.noreply.github.com>
2021-11-28 09:36:10 -0500

    Create exercise.py

---
+++ Udemy Material/Airline Satisfaction/exercise.py
@@ -0,0 +1,35 @@
+#Add Key imports here!
+
+
+def read_train_dataset():
+    """
+    This function should read in the train.csv and return it 
+    in whatever representation you like
+    """
+    
+    ###YOUR CODE HERE####
+    raise NotImplementedError("Did not implement read_train_dataset() function")
+    #####################
+
+def preprocess_dataset():
+    """
+    Given the raw dataset read in from your read_train_dataset() function,
+    
+    process the dataset accordingly 
+    """
+    ###YOUR CODE HERE####
+    raise NotImplementedError("Did not implement read_train_dataset() function")
+    #####################
+
+def train_model():
+    """
+    Given your cleaned data, train your Machine Learning model on it and return the
+    model
+    
+    MANDATORY FUNCTION TO IMPLEMENT
+    """
+    
+    ####YOUR CODE HERE#####
+    raise NotImplementedError("Did not implement the train_model() function")
+    #######################
+    
