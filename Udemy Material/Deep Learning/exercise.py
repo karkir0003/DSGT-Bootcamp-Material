dc6cef04fc3e4e5fa923913093c3109d3d9ace8d
karkir0003 <54720987+karkir0003@users.noreply.github.com>
2022-01-15 13:16:03 -0500

    Create exercise.py

---
+++ Udemy Material/Deep Learning/exercise.py
@@ -0,0 +1,8 @@
+Assignment Instructions
+To get started, simply click the assignment link given in the first question and follow prompts as given inside of the google Collaboratory notebook.
+
+Assignment Link: Intro to Neural Networks
+
+Once you are finished, simply paste in your copy of the notebook with your implementation included.
+
+Optional: You can click on this link to do a simple guided exercise to help you get familiar with the material inside of the assignment.
